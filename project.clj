a1399f47178ca0c03ff8ec9e7c05be6acf055775
Brian L. Troutwine <brian@troutwine.us>
2014-03-15 14:54:42 -0700

    Fixed everything about this.
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject smug-core "1.0.0-FINAL"
-  :description "smug-core"
-  :author "Mike Sassak"
-  :url "http://github.com/msassak/smug-core"
-  :license {:name "GNU Affero General Public License v3"
-            :url "http://www.gnu.org/licenses/agpl-3.0.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
